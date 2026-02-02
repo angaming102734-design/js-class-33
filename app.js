3adc561f171f14879d72f7f0f847477352455de4
ather ali <a.ngaming102734@gmail.com>
2026-02-02 19:39:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log("aslam walikum")
+
+// let perant =document.childNodes[1].childNodes[2].childNodes;
+// console.log('parent',parent)
+
+// let div=document.getElementById('ny')
+// let choang =div.childNodes[1];
+// p.innerHTML='Hello my Love'
+
+        let div
